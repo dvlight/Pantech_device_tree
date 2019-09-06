fed84a8747d9d6a8e0ae924114a4f09f564abd98
Dev-light1234 <shinsooan0610@gmail.com>
2019-09-06 16:39:39 +0900

    delete old things

--- qcom/sepolicy.mk
+++
@@ -1,6 +0,0 @@
-# Board specific SELinux policy variable definitions
-BOARD_SEPOLICY_DIRS := \
-       device/qcom/sepolicy \
-       device/qcom/sepolicy/common \
-       device/qcom/sepolicy/test \
-       device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
