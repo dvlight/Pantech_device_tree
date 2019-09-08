aced5c44b93d2ebcc4cb51916681f137f19f9d18
Dev-light1234 <shinsooan0610@gmail.com>
2019-09-08 20:55:46 +0900

    moved to aosp_ef52l.mk

--- pantech/ef52l/aosp.mk
+++ pantech/ef52l/aosp.mk
@@ -1,24 +1,2 @@
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1080
-TARGET_SCREEN_WIDTH := 720
-
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/aosp/config/commom.mk
-
-# Inherit device configuration
-$(call inherit-product, device/pantech/ef52l/aosp_ef52l.mk)
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := ef52l
-PRODUCT_NAME := aosp_ef52l
-PRODUCT_BRAND := VEGA
-PRODUCT_MODEL := Vega Iron
-PRODUCT_MANUFACTURER := Pantech
-
-#PRODUCT_BUILD_PROP_OVERRIDES += \
-#    PRODUCT_NAME="Vega Iron" \
-#    TARGET_DEVICE=ef52l \
-
-# Enable Torch
-#PRODUCT_PACKAGES += Torch
-
