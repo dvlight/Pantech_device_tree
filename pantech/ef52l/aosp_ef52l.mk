a26b780981858b4d743ef6ab629bd3339644e475
Dev-light1234 <shinsooan0610@gmail.com>
2019-09-08 22:06:51 +0900

    use aosp vendor

--- pantech/ef52l/aosp_ef52l.mk
+++ pantech/ef52l/aosp_ef52l.mk
@@ -14,15 +14,14 @@
 # limitations under the License.
 #
 
-
-# Sample: This is where we'd set a backup provider if we had one
-# $(call inherit-product, device/sample/products/backup_overlay.mk)
-
 # Get the long list of APNs
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/commom.mk
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/pantech/ef52l/device.mk)
@@ -33,6 +32,9 @@ PRODUCT_BRAND := VEGA
 PRODUCT_MODEL := Vega Iron
 PRODUCT_MANUFACTURER := Pantech
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Use Magisk
+DEFAULT_ROOT_METHOD := magisk
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1080
