bb2322f63a4d5a5cd56bbf9cc1b97e166522c528
Dev-light1234 <shinsooan0610@gmail.com>
2019-09-09 21:34:25 +0900

    old things removed

--- pantech/msm8960-common/msm8960.mk
+++ pantech/msm8960-common/msm8960.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011 The Android Open-Source Project
+# Copyright (C) 2019 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -175,8 +175,6 @@ PRODUCT_PACKAGES += \
 	ueventd.qcom.rc \
 	preinstall.sh \
 	initlogo.rle \
-	battery_fail.png \
-	battery_scale.png
 
 #Camera
 PRODUCT_PACKAGES += \
