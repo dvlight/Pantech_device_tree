4111bb5f42f9d8492f0f6167b1f870e16c695cbb
Dev-light1234 <shinsooan0610@gmail.com>
2019-10-28 22:33:30 +0900

    LOCAL_PATH

--- pantech/ef52l/Android.mk
+++ pantech/ef52l/Android.mk
@@ -1,4 +1,6 @@
-ifeq (ef52l,$(TARGET_DEVICE))
+LOCAL_PATH := $(call my-dir)
+
+ifeq (ef52 ef52l ef52s ef52k,$(TARGET_DEVICE))
 LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
