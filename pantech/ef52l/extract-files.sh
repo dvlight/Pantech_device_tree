0bb778a7aae2a0c4df9c8d865802f38407d43c8e
Dev-light1234 <shinsooan0610@gmail.com>
2019-10-28 22:43:30 +0900

    rename && cleanup

--- pantech/ef52l/extract-files.sh
+++ pantech/ef52l/extract-files.sh
@@ -1,17 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-VENDOR=pantech
-DEVICE=ef52l
+set -e
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
+export DEVICE=ef52l
+export DEVICE_COMMON=msm8960-common
+export VENDOR=pantech
 
-for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ `; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    adb pull /system/$FILE $BASE/$FILE
-done
-./copy_from_target.sh
-./setup-makefiles.sh
+./../$DEVICE_COMMON/extract-files.sh $@
